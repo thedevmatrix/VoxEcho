ac303788f6bf6faaa4860fad23f752c02605c04d
Dev Matrix <thedevmatrix5@gmail.com>
2025-04-25 16:42:06 +0100

    Revert "created a load  global config file for environment"

--- VoxEcho-Workspace/apps/backend-voxecho/src/app/app.module.ts
+++ VoxEcho-Workspace/apps/backend-voxecho/src/app/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { envSchema } from '../config/validation';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-      validate: (config) => {
-        const parsed = envSchema.safeParse(config);
-        if (!parsed.success) {
-          throw new Error(`❌ Invalid environment variables: ${parsed.error.message}`);
-        }
-        return parsed.data;
-      },
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- VoxEcho-Workspace/apps/backend-voxecho/src/config/validation.ts
+++
@@ -1,8 +0,0 @@
-import {z} from "zod";
-
-export const envSchema = z.object ({
-    PORT: z.string().default('3000'),
-    DATABASE_URL: z.string().url(),
-    JWT_SECRET: z.string().min(1),
-
-});
